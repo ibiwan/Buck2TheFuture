8bfb7a2de6ba7699a53d0102a9dc8efd2169e192
Jeremy Kent <ibiwan@gmail.com>
2012-09-05 22:30:29 -0700

    Initial Commit

---
+++ Buck 2 the Future/Buck2ViewController.h
@@ -0,0 +1,13 @@
+//
+//  Buck2ViewController.h
+//  Buck 2 the Future
+//
+//  Created by Jeremiah Kent on 9/5/12.
+//  Copyright (c) 2012 Jeremiah Kent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Buck2ViewController : UIViewController
+
+@end
